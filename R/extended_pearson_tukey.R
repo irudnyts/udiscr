ed13f52bba91b36dbe8926a3ac8d175031e2e4e4
irudnyts <yegor.rudnytskiy@gmail.com>
2017-02-02 16:18:54 +0100

    Add extended Pearson-Tukey method

---
+++ R/extended_pearson_tukey.R
@@ -0,0 +1,14 @@
+extended_pearson_tukey <- function(q_fun, params) {
+    q <- function(p) {
+        args <- params
+        args[["p"]] <- p
+        do.call(what = q_fun, args = args)
+    }
+
+    return(list(list(prob = 0.185,
+                     point = q(0.05)),
+                list(prob = 0.630,
+                     point = q(0.5)),
+                list(prob = 0.185,
+                     point = q(0.95))))
+}
