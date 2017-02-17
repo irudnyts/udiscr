dce5333b931651d3560e291c220c4213b5999e37
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2017-02-17 22:06:40 +0100

    Finish BKP-5 method, add documentation and tests

--- R/brown_kahr_peterson.R
+++ R/brown_kahr_peterson.R
@@ -50,10 +50,10 @@ brown_kahr_peterson_3 <- function(q_fun, params) {
 }
 
 
-#' Three-point approximation, proposed by Brown, Kahr, and Peterson
+#' Five-point approximation, proposed by Brown, Kahr, and Peterson
 #'
 #' Returns a discrete approximation of a continuous distribution using
-#' three-point approximation, proposed by Brown, Kahr, and Peterson.
+#' five-point approximation, proposed by Brown, Kahr, and Peterson.
 #'
 #' Note: only distribution with finite quantiles are supported.
 #'
@@ -62,15 +62,15 @@ brown_kahr_peterson_3 <- function(q_fun, params) {
 #' @return a list of pairs probability-outcome.
 #'
 #' @examples
-#' brown_kahr_peterson_3(q_fun = qbeta, params = list(shape1 = 2, shape2 = 3))
-#' brown_kahr_peterson_3(q_fun = qunif)
+#' brown_kahr_peterson_5(q_fun = qbeta, params = list(shape1 = 2, shape2 = 3))
+#' brown_kahr_peterson_5(q_fun = qunif)
 #' # using sample data
 #' sample <- rnorm(100) + rexp(100)
 #' q <- function(p) quantile(x = sample, probs = p)
-#' brown_kahr_peterson_3(q_fun = q)
+#' brown_kahr_peterson_5(q_fun = q)
 #'
 #' \dontrun{
-#' brown_kahr_peterson_3(q_fun = qnorm)
+#' brown_kahr_peterson_5(q_fun = qnorm)
 #' }
 #'
 #' @references \itemize{
@@ -93,14 +93,17 @@ brown_kahr_peterson_5 <- function(q_fun, params) {
 
     v2 <- q(0.25); p2 <- 0.5 * (q(0.5) - q(0.25)) / (q(0.25) - q(0))
     v4 <- q(0.75); p4 <- 0.5 * (q(0.75) - q(0.5)) / (q(1) - q(0.75))
+    v1 <- q(0.25) - 0.25 * (1 + 2 * p2) * (q(0.5) - q(0))
+    p1 <- 0.25 * (1 - 2 * p2)
+    v5 <- q(0.75) + 0.25 * (1 + 2 * p4) * (q(1) - q(0.5))
+    p5 <- 0.25 * (1 - 2 * p4)
+    s <- 0.5 * (q(0.25) + q(0.5)) + 0.5 * p2 * (q(0.5) - q(0))
+    t <- 0.5 * (q(0.5) + q(0.75)) + 0.5 * p4 * (q(1) - q(0.5))
+    p3 <- p1 + p5; v3 <- (s * p1 + t * p5) / p3
 
-    v1 <- q(0.25) - 0.25 * (1 + 2 * p2)
-    stop("to be impelemented...")
-
-    return(list(list(prob = 0.25,
-                     point = (3 * q(0) + 5 * q(0.5)) / 8),
-                list(prob = 0.5,
-                     point = (q(0) + 14 * q(0.5) + q(1)) / 16),
-                list(prob = 0.25,
-                     point = (5 * q(0.5) + 3 * q(1)) / 8)))
+    return(list(list(prob = p1, point = v1),
+                list(prob = p2, point = v2),
+                list(prob = p3, point = v3),
+                list(prob = p4, point = v4),
+                list(prob = p5, point = v5)))
 }
--- tests/testthat/test-brown_kahr_peterson.R
+++ tests/testthat/test-brown_kahr_peterson.R
@@ -11,7 +11,7 @@ test_that("brown_kahr_peterson_3 checks argument params on validity corretly", {
 })
 
 test_that("brown_kahr_peterson_3 returns corret result", {
-    # exponential
+    # uniform
     rslt_3 <- list(list(prob = 0.25,
                         point = (3 * qunif(0) + 5 * qunif(0.5)) / 8),
                    list(prob = 0.5,
@@ -35,5 +35,70 @@ test_that("brown_kahr_peterson_3 returns corret result", {
     expect_equal(brown_kahr_peterson_3(q_fun = qbeta, params = list(shape1 = shape1,
                                                                     shape2 = shape2)),
                  rslt_4)
+
+})
+
+test_that("brown_kahr_peterson_5 checks argument params on validity corretly", {
+    expect_silent(brown_kahr_peterson_5(q_fun = qunif, params = list()))
+    expect_silent(brown_kahr_peterson_5(q_fun = qunif))
+    expect_silent(brown_kahr_peterson_5(q_fun = qbeta, params = list(shape1 = 4,
+                                                                     shape2 = 3)))
+    expect_error(brown_kahr_peterson_5(q_fun = qbeta, params = list(a = 1)))
+    expect_error(brown_kahr_peterson_5(q_fun = qnorm))
+    expect_error(brown_kahr_peterson_5(q_fun = qexp))
+})
+
+test_that("brown_kahr_peterson_5 returns corret result", {
+    # uniform
+    min_ <- 2; max_ <- 10
+    q_unif <- function(x) qunif(x, min = min_, max = max_)
+
+    v2 <- q_unif(0.25)
+    p2 <- 0.5 * (q_unif(0.5) - q_unif(0.25)) / (q_unif(0.25) - q_unif(0))
+    v4 <- q_unif(0.75)
+    p4 <- 0.5 * (q_unif(0.75) - q_unif(0.5)) / (q_unif(1) - q_unif(0.75))
+    v1 <- q_unif(0.25) - 0.25 * (1 + 2 * p2) * (q_unif(0.5) - q_unif(0))
+    p1 <- 0.25 * (1 - 2 * p2)
+    v5 <- q_unif(0.75) + 0.25 * (1 + 2 * p4) * (q_unif(1) - q_unif(0.5))
+    p5 <- 0.25 * (1 - 2 * p4)
+    s <- 0.5 * (q_unif(0.25) + q_unif(0.5)) + 0.5 * p2 * (q_unif(0.5) - q_unif(0))
+    t <- 0.5 * (q_unif(0.5) + q_unif(0.75)) + 0.5 * p4 * (q_unif(1) - q_unif(0.5))
+    p3 <- p1 + p5; v3 <- (s * p1 + t * p5) / p3
+
+    rslt_1 <- list(list(prob = p1, point = v1),
+                   list(prob = p2, point = v2),
+                   list(prob = p3, point = v3),
+                   list(prob = p4, point = v4),
+                   list(prob = p5, point = v5))
+    # beta
+    shape1 <- 3; shape2 <- 5
+    q_beta <- function(x) qbeta(x, shape1 = shape1, shape2 = shape2)
+
+    v2 <- q_beta(0.25)
+    p2 <- 0.5 * (q_beta(0.5) - q_beta(0.25)) / (q_beta(0.25) - q_beta(0))
+    v4 <- q_beta(0.75)
+    p4 <- 0.5 * (q_beta(0.75) - q_beta(0.5)) / (q_beta(1) - q_beta(0.75))
+    v1 <- q_beta(0.25) - 0.25 * (1 + 2 * p2) * (q_beta(0.5) - q_beta(0))
+    p1 <- 0.25 * (1 - 2 * p2)
+    v5 <- q_beta(0.75) + 0.25 * (1 + 2 * p4) * (q_beta(1) - q_beta(0.5))
+    p5 <- 0.25 * (1 - 2 * p4)
+    s <- 0.5 * (q_beta(0.25) + q_beta(0.5)) + 0.5 * p2 * (q_beta(0.5) - q_beta(0))
+    t <- 0.5 * (q_beta(0.5) + q_beta(0.75)) + 0.5 * p4 * (q_beta(1) - q_beta(0.5))
+    p3 <- p1 + p5; v3 <- (s * p1 + t * p5) / p3
+
+    rslt_2 <- list(list(prob = p1, point = v1),
+                   list(prob = p2, point = v2),
+                   list(prob = p3, point = v3),
+                   list(prob = p4, point = v4),
+                   list(prob = p5, point = v5))
+
+
+    expect_equal(brown_kahr_peterson_5(q_fun = qunif,
+                                       list(min = min_, max = max_)),
+                 rslt_1)
+    expect_equal(brown_kahr_peterson_5(q_fun = qbeta,
+                                       params = list(shape1 = shape1,
+                                                     shape2 = shape2)),
+                 rslt_2)
 
 })
